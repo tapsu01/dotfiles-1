234fcac609bc83b6a556acb39289cbdcbb94d5e4
lioaslan <arey.tieuleo.5.7@gmail.com>
2021-05-26 22:54:17 +0700

    Update powermenu index

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-cp .zshrc ~/.zshrc
-# cp -r .config/bspwm ~/.config/
-# cp -r .config/sxhkd/ ~/.config/
+# cp .zshrc ~/.zshrc
+# source ~/.zshrc
+
+# cp -R .config/bspwm ~/.config/
+# cp -R .config/sxhkd/ ~/.config/
+cp -R .config/rofi ~/.config/
 
 echo 'Copying successfully'
-source ~/.zshrc
