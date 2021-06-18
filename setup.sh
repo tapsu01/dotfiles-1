662737bb69c9203f1f0e3ece81c14bf549d4759d
lioaslan <leoalan5577@gmail.com>
2021-06-18 23:07:32 +0700

    Update mpv config

--- setup.sh
+++ setup.sh
@@ -3,12 +3,7 @@
 cp .zshrc ~/.zshrc
 source ~/.zshrc
 
-cp -R .config/nvim ~/.config
-cp -R .config/bspwm ~/.config/
-cp -R .config/sxhkd/ ~/.config/
-cp -R .config/kitty ~/.config
-cp -R .config/onedrive ~/.config
-cp -R .config/dunst ~/.config
+cp -R .config/* ~/.config
 
 cp images/* ~/Pictures
 
